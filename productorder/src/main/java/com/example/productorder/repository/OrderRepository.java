0bad883f9062530f91d8c3fecf79ffbc60983ad9
mahagrandhi30 <mahagrandhi30@gmail.com>
2025-08-04 22:42:52 +0530

    Removed leftover productorder folder after renaming

--- productorder/src/main/java/com/example/productorder/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productorder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.productorder.entity.OrderEntity;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-}
--- productorder/src/main/java/com/example/productorder/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productorder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.productorder.entity.ProductEntity;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
